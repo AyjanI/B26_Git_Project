edcabd8e089e3bc6d25b98a0b603ed85b23a2b65
markushka <merdanaya@gmail.com>
2022-03-26 20:31:58 -0700

    Fixed the typo

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -1,8 +1,8 @@
-package day0:
+package day0;
 
 public class JamesBond{
   
-  publis static void main(String [] args){
+  public static void main(String [] args){
     System.out.println("James Bond hacked the project");
   }
 }
